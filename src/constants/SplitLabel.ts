2bde8ad4d02c13b84d03894f528b60acc97dcc94
amh6281 <amh6281@naver.com>
2025-07-05 17:03:04 +0900

    feat: 분할, 루틴 constants 추가

---
+++ src/constants/SplitLabel.ts
@@ -0,0 +1,23 @@
+export const SPLIT_LABELS = {
+  '3-split': '3분할',
+  '4-split': '4분할',
+  '5-split': '5분할',
+  'full-body': '무분할 (전신)',
+  custom: '커스텀',
+};
+
+export const ROUTINE_LABELS = {
+  rest: '휴무',
+  chest: '가슴',
+  back: '등',
+  legs: '하체',
+  shoulders: '어깨',
+  biceps: '이두',
+  triceps: '삼두',
+  'chest-triceps': '가슴/삼두',
+  'back-biceps': '등/이두',
+  'chest-shoulders': '가슴/어깨',
+  'full-body': '전신',
+  abs: '복부',
+  cardio: '유산소',
+};
