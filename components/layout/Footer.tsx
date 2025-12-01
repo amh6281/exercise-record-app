97695e1f1a76a1c86e3011e4b8cc372c3ad619ff
amh6281 <amh6281@naver.com>
2025-12-01 19:05:24 +0900

    feat: layout 구성

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="border-t bg-white/80 py-4 text-center text-xs text-zinc-500 dark:bg-black/60">
+      © {new Date().getFullYear()} FitFlow. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
